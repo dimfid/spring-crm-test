293816e48728c87c9dd5ef8b5842d44ddcbc3fe1
Moohlas <moohlas@gmail.com>
2019-06-24 15:16:15 +0300

    added method addCustomer to CustomerRrestController

--- spring-crm-rest/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ spring-crm-rest/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,6 +41,20 @@ public class CustomerRestController {
 			throw new CustomerNotFoundException("Customer not found - " + customerId);
 		}
 		
+		return theCustomer;
+	}
+	
+	//add mapping for POST /customers -add new customer
+	
+	@PostMapping("/customers")
+	public Customer addCustomer(@RequestBody Customer theCustomer) {
+		
+		//also just in case the pass an id in Json... set id to 0
+		//this forces a save of new item... instead of update
+		
+		theCustomer.setId(0);
+		
+		customerService.saveCustomer(theCustomer);
 		return theCustomer;
 	}
 }
